49582ba49236df2f8a192bd1e06aa668c4975e25
Niko Raisanen <nraisane@asu.edu>
2021-04-29 21:02:55 -0700

    finished regionMap

--- test.go
+++ test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -70,23 +71,22 @@ func parseRequest(body string) ([]string, []string) {
 		}
 	})
 	var sliceElements []string
+
 	// Get name of each body of water
 	doc.Find("p strong").Each(func(i int, s *goquery.Selection) {
 		txt := s.Text()
-		// fmt.Println(txt)
 		sliceElements = append(sliceElements, txt)
 	})
+
 	// Bodies of water that do not have reports for any reason
 	for i := 0; i < len(sliceElements)-1; i++ {
-		// fmt.Printf("%d %v\n", i, sliceElements[i])
 		if strings.Contains(sliceElements[i+1], "No") {
-			// fmt.Printf("%v should not have a report\n", sliceElements[i])
+			// pass
 		} else if len(sliceElements[i]) <= 5 || strings.Contains(sliceElements[i], "No") ||
 			strings.Contains(sliceElements[i], "Cluff Ranch") {
-			// fmt.Println("Less than 5 chars.")
+			// pass
 		} else {
 			waters = append(waters, sliceElements[i])
-			// fmt.Printf("Else block %v\n", sliceElements[i])
 		}
 	}
 	fmt.Println(waters)
@@ -117,21 +117,28 @@ func mapRatings(ratings []string) []string {
 	return hr
 }
 
-func prepData(r []string, w []string) ([15][]string, int) {
-	// var data []string
+func prepData(r []string, w []string, ref string) ([15][]string, int) {
+	// Set region based off of ref
 	var dataSlice [15][]string
+	regionMap := map[string]string{
+		"https://www.azgfd.com/fishing/forecast/se-central-az/#se-az":        "Southeast Arizona",
+		"https://www.azgfd.com/fishing/forecast/#central":                    "Central Arizona",
+		"https://www.azgfd.com/fishing/forecast/mogollon-rim/#mogollon":      "Mogollon Rim",
+		"https://www.azgfd.com/fishing/forecast/colorado-river/#colorado-nw": "NW Colorado River",
+	}
+
 	w = w[0:len(r)]
 	for i := 0; i < len(w); i++ {
 		name := w[i]
 		rating := r[i]
 		iString := strconv.Itoa(i)
-		dbLine := newEntry(iString, name, rating, "saturday", "az")
+		current_time := time.Now().Format(time.RFC3339)
+		dbLine := newEntry(iString, name, rating, current_time, regionMap[ref])
 		fmt.Println(dbLine)
 		dataSlice[i] = append(dataSlice[i], dbLine.number, dbLine.name,
 			dbLine.rating, dbLine.time, dbLine.region)
 	}
-	// fmt.Println(data)
-	// fmt.Println(dataSlice)
+
 	// Remove empty arrays
 	var counter int
 	for _, array := range dataSlice {
@@ -159,38 +166,41 @@ func aggregateData(d [][]string, d2 [][]string, d3 [][]string, d4 [][]string) []
 }
 
 func main() {
-	// var finalData [][]string
 	// Start request #1
-	results := makeRequest("https://www.azgfd.com/fishing/forecast/se-central-az/#se-az")
+	url1 := "https://www.azgfd.com/fishing/forecast/se-central-az/#se-az"
+	results := makeRequest(url1)
 	sliceRatings, sliceWaters := parseRequest(results)
-	entryData, counter := prepData(sliceRatings, sliceWaters)
+	entryData, counter := prepData(sliceRatings, sliceWaters, url1)
 	fmt.Println(entryData)
 	cleanData := append(entryData[0 : len(entryData)-counter][:])
 	fmt.Println(cleanData)
 	fmt.Printf("The data has length: %d\nCounter is %d\n", len(cleanData), counter)
 	// End request #1
 	// Start request #2
-	results2 := makeRequest("https://www.azgfd.com/fishing/forecast/#central")
+	url2 := "https://www.azgfd.com/fishing/forecast/#central"
+	results2 := makeRequest(url2)
 	sliceRatings2, sliceWaters2 := parseRequest(results2)
-	entryData2, counter2 := prepData(sliceRatings2, sliceWaters2)
+	entryData2, counter2 := prepData(sliceRatings2, sliceWaters2, url2)
 	fmt.Println(entryData2)
 	cleanData2 := append(entryData2[0 : len(entryData2)-counter2][:])
 	fmt.Println(cleanData2)
 	fmt.Printf("The data has length: %d\nCounter is %d\n", len(cleanData2), counter2)
 	// End request #2
 	// Start request #3
-	results3 := makeRequest("https://www.azgfd.com/fishing/forecast/mogollon-rim/#mogollon")
+	url3 := "https://www.azgfd.com/fishing/forecast/mogollon-rim/#mogollon"
+	results3 := makeRequest(url3)
 	sliceRatings3, sliceWaters3 := parseRequest(results3)
-	entryData3, counter3 := prepData(sliceRatings3, sliceWaters3)
+	entryData3, counter3 := prepData(sliceRatings3, sliceWaters3, url3)
 	fmt.Println(entryData3)
 	cleanData3 := append(entryData3[0 : len(entryData3)-counter3][:])
 	fmt.Println(cleanData3)
 	fmt.Printf("The data has length: %d\nCounter is %d\n", len(cleanData3), counter3)
 	// End request #3
 	// Start request #4
-	results4 := makeRequest("https://www.azgfd.com/fishing/forecast/colorado-river/#colorado-nw")
+	url4 := "https://www.azgfd.com/fishing/forecast/colorado-river/#colorado-nw"
+	results4 := makeRequest(url4)
 	sliceRatings4, sliceWaters4 := parseRequest(results4)
-	entryData4, counter4 := prepData(sliceRatings4, sliceWaters4)
+	entryData4, counter4 := prepData(sliceRatings4, sliceWaters4, url4)
 	fmt.Println(entryData4, counter4)
 	cleanData4 := append(entryData4[0 : len(entryData4)-counter4][:])
 	fmt.Println(cleanData4)
