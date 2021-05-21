68760e3a7921b79c22c338b020768bb08f6d1262
root <nraisane@asu.edu>
2021-05-20 19:47:05 -0700

    js output

--- frontEnd/index.php
+++ frontEnd/index.php
@@ -9,6 +9,14 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 <body onload="execute_this()">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     <!-- <script>
         // function reqListener () {
         // console.log(this.responseText);
@@ -37,7 +45,7 @@
     <h1 id="mainHeader">Niko's AZ Fishing Guide</h1>
         <input id="clickMe" type="button" value="clickme" onclick="populate_results()">
     <div id="content">
-        <p id="testcontent">Hello</p>
+        <p id="testcontent">This is testcontent</p>
         <!-- Use js to populate this -->
     </div>
     <div id="root"></div>
--- frontEnd/script.js
+++ frontEnd/script.js
@@ -38,8 +38,49 @@ function printing_var() {
 }
 
 function populate_results() {
+    const app = document.getElementById('root');
+    const logo = document.createElement('img');
+    logo.src = 'lake.jpg';
+    const container = document.createElement('div');
+    container.setAttribute('class', 'container');
+
+    app.appendChild(logo);
+    app.appendChild(container);
+    var output = '';
     for (var entry of allData) {
         console.log(entry.id);
-        document.getElementById("testcontent").innerHTML = entry.id;
+        output += entry.rating;
+
+        id = entry.id;
+        rating = entry.rating;
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+
+        const h1 = document.createElement('h1');
+        h1.textContent = id;
+
+        const p = document.createElement('p');
+        p.textContent = rating;
+
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(p);
+        // textfield = document.createElement('p');
+        // textfield.textContent = 'HELLOOOOOOOOOOOOO';
+        // card = document.createElement('div');
+        // card.setAttribute('class', 'card');
+        // card.setAttribute('style', 'width: 18rem;');
+        // img = document.createElement('img');
+        // img.setAttribute('src', 'lake.jpg');
+        // cbody = document.createElement('div');
+        // cbody.setAttribute('class', 'card-body');
+
+        
+
+        // contentArea = document.getElementById('content');
+        // contentArea.appendChild(card);
+        // contentArea.appendChild(img);
+        // contentArea.appendChild(cbody);
     }
+    document.getElementById("testcontent").innerHTML = output;
 }
