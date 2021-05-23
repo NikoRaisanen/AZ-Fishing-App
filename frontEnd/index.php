4033492241d44c813a744b2f23ae344abbefe310
root <nraisane@asu.edu>
2021-05-22 21:23:06 -0700

    ordering by select menu

--- frontEnd/index.php
+++ frontEnd/index.php
@@ -11,44 +11,27 @@
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
-<body onload="execute_this()">
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    <!-- <script>
-        // function reqListener () {
-        // console.log(this.responseText);
-        // }
+<body onload="set_allData()">
+    <!-- Create big button that says "Get started" 
+    also show text explaining what the app is-->
 
-        var oReq = new XMLHttpRequest(); // New request object
-        oReq.onload = function() {
-            // This is where you handle what to do with the response.
-            // The actual data is found on this.responseText
-            alert(this.responseText); // Will alert: 42
-
-            // Convert data from php to javascript
-            var data = this.responseText;
-            console.log(data);
-            var dataArray = data.split("\n");
-
-
-
-        };
-        oReq.open("get", "../backEnd/phpToJs.php", true);
-        //                               ^ Don't block the rest of the execution.
-        //                                 Don't wait until the request finishes to
-        //                                 continue.
-        oReq.send();
-    </script> -->
     <h1 id="mainHeader">Niko's AZ Fishing Guide</h1>
-        <input id="clickMe" type="button" value="clickme" onclick="populate_results()">
-        <input id="orderbyRating" type="button" value="Order by rating" onclick="order_by_rating()">
-        <input id="resetallData" type="button" value="Reset allData" onclick="reset_allData()">
+        <input id="getstarted" type="button" value="Get started!" onclick="order_by_name()">
+        <p id="intro">Hello this is my intro paragraph!</p>
+        <!-- <input id="orderbyRating" type="button" value="Order by rating" onclick="order_by_rating()">
+        <input id="orderbyName" type="button" value="Order by name" onclick="order_by_name()">
+        <input id="orderbyRegion" type="button" value="Order by region" onclick="order_by_region()">
+        <input id="resetallData" type="button" value="Reset allData" onclick="reset_allData()"> -->
+        <div id="searchdiv" class='centerdata' style='display:none;'>
+            <label for="search">Order by:</label>
+            <select id="search" name="search" onchange="update_search(value)">
+                <option value="name">Name</option>
+                <option value="rating">Rating</option>
+                <option value="region">Region</option>
+            </select>
+        </div>
+
+
     <div id="content">
         <p id="testcontent">This is testcontent</p>
         <!-- Use js to populate this -->
--- frontEnd/script.js
+++ frontEnd/script.js
@@ -27,7 +27,7 @@ function get_data() {
         // return dataArray;
 }
 
-function execute_this() {
+function set_allData() {
     get_data();
     console.log(allData);
     console.log(testvar);
@@ -38,6 +38,13 @@ function printing_var() {
 }
 
 function populate_results() {
+    remove_output();    // Clear current results if they exist
+    search = document.getElementById('searchdiv');
+    search.style.display = "block";
+    document.getElementById('intro').innerText = "";
+    getstarted = document.getElementById('getstarted');
+    getstarted.style.visibility = "hidden";
+
     var output = '';
     for (var entry of allData) {
         console.log(entry.id);
@@ -158,8 +165,12 @@ function populate_results() {
 // Resets allData variable and clears generated output
 function reset_allData() {
     get_data();
+}
+
+function remove_output () {
     document.getElementById("content").innerHTML = "";
 }
+
 function order_by_rating() {
     console.log('THis is the order by region function: \n' + JSON.stringify(allData));
     for (var i = 0; i < allData.length; i++) {
@@ -296,5 +307,291 @@ function order_by_rating() {
         // Add line break to differentiate cards
         br = document.createElement('br');
         contentArea.appendChild(br);
+        reset_allData();
     }
+}
+
+function order_by_name() {
+    remove_output();    // Clear current results if they exist
+    search = document.getElementById('searchdiv');
+    search.style.display = "block";
+    document.getElementById('intro').innerText = "";
+    getstarted = document.getElementById('getstarted');
+    getstarted.style.visibility = "hidden";
+
+    console.log('THis is the order by region function: \n' + JSON.stringify(allData));
+    for (var i = 0; i < allData.length; i++) {
+        console.log(i)
+        console.log(allData[i]);
+    }
+    // Sort alphabetically by name
+    allData.sort(function(a, b) {
+        return a.name > b.name;
+    })
+    // Put "Poor" ratings to beginning
+    console.log(allData);
+
+    for (var entry of allData) {
+        console.log(entry.id);
+
+        id = entry.id;
+        rating = entry.rating;
+        card = document.createElement('div');
+        card.setAttribute('class', 'card centerdata');
+        // Modify the below width so that all content scales
+        card.setAttribute('style', 'width: 30rem');
+        img = document.createElement('img');
+
+
+        // Switch to get proper image src
+        switch(entry.name) {
+            case 'Colorado River ':
+                img.setAttribute('src', 'images/coloradoRiver.jpg');
+                break;
+            case 'Arivaca Lake ':
+                img.setAttribute('src', 'images/arivacaLake.jpg');
+                break;
+            case 'Dankworth Pond ':
+                img.setAttribute('src', 'images/dankworthPond.jpg');
+                break;
+            case 'Frye Mesa Reservoir ':
+                img.setAttribute('src', 'images/frymesaReservoir.jpg');
+                break;
+            case 'Parker Canyon Lake ':
+                img.setAttribute('src', 'images/parkercanyonLake.jpg');
+                break;
+            case 'Apache Lake ':
+                img.setAttribute('src', 'images/apacheLake.jpg');
+                break;
+            case 'Bartlett Lake ':
+                img.setAttribute('src', 'images/bartlettLake.jpg');
+                break;
+            case 'Canyon Lake ':
+                img.setAttribute('src', 'images/canyonLake.jpg');
+                break;
+            case 'Lake Pleasant ':
+                img.setAttribute('src', 'images/lakePleasant.jpg');
+                break;
+            case 'Lower Salt River ':
+                img.setAttribute('src', 'images/lowersaltRiver.jpg');
+                break;
+            case 'Roosevelt Lake ':
+                img.setAttribute('src', 'images/rooseveltLake.jpg');
+                break;
+            case 'Saguaro Lake ':
+                img.setAttribute('src', 'images/saguaroLake.jpg');
+                break;
+            case 'Tempe Town Lake* ':
+                img.setAttribute('src', 'images/tempetownLake.jpg');
+                break;
+            case 'Bear Canyon Lake ':
+                img.setAttribute('src', 'images/bearcanyonLake.jpg');
+                break;
+            case 'Black Canyon Lake ':
+                img.setAttribute('src', 'images/blackcanyonLake.jpg');
+                break;
+            case 'Chevelon Canyon ':
+                img.setAttribute('src', 'images/chevelonCanyon.jpg');
+                break;
+            case 'Willow Springs Lake ':
+                img.setAttribute('src', 'images/willowspringsLake.jpg');
+                break;
+            case 'Woods Canyon Lake ':
+                img.setAttribute('src', 'images/woodscanyonLake.jpg');
+                break;
+            case 'Becker Lake ':
+                img.setAttribute('src', 'images/beckerLake.jpg');
+                break;
+            case 'Big Lake ':
+                img.setAttribute('src', 'images/bigLake.jpg');
+                break;
+            case 'Greer Lakes ':
+                img.setAttribute('src', 'images/greerLakes.jpg');
+                break;
+            case 'Carnero Lake ':
+                img.setAttribute('src', 'images/carneroLake.jpg');
+                break;
+            default:
+                console.log('default switch executed');
+                img.setAttribute('src', 'images/saguaroLake.jpg');
+        }
+
+
+        img.setAttribute('class', 'card-img-top');
+        cbody = document.createElement('div');
+        cbody.setAttribute('class', 'card-body');
+        h5 = document.createElement('h5');
+        h5.setAttribute('class', 'card-title');
+        h5.textContent = entry.name;
+        p1 = document.createElement('p');
+        p1.setAttribute('class', 'card-text');
+        p1.textContent = 'Region: ' + entry.region;
+        p2 = document.createElement('p');
+        p2.setAttribute('class', 'card-text');
+        p2.textContent = 'Fishing rating: ' + entry.rating;
+
+
+        card.appendChild(img);
+        cbody.appendChild(h5);
+        cbody.appendChild(p1);
+        cbody.appendChild(p2);
+        card.appendChild(cbody);
+
+        contentArea = document.getElementById('content');
+
+        contentArea.appendChild(card);
+        // Add line break to differentiate cards
+        br = document.createElement('br');
+        contentArea.appendChild(br);
+        reset_allData();
+    }
+}
+
+function order_by_region() {
+    document.getElementById("content").innerHTML = " ";
+    console.log('THis is the order by region function: \n' + JSON.stringify(allData));
+    for (var i = 0; i < allData.length; i++) {
+        console.log(i)
+        console.log(allData[i]);
+    }
+    // Sort alphabetically by name
+    allData.sort(function(a, b) {
+        return a.region > b.region;
+    })
+    // Put "Poor" ratings to beginning
+    console.log(allData);
+
+    for (var entry of allData) {
+        console.log(entry.id);
+
+        id = entry.id;
+        rating = entry.rating;
+        card = document.createElement('div');
+        card.setAttribute('class', 'card centerdata');
+        // Modify the below width so that all content scales
+        card.setAttribute('style', 'width: 30rem');
+        img = document.createElement('img');
+
+
+        // Switch to get proper image src
+        switch(entry.name) {
+            case 'Colorado River ':
+                img.setAttribute('src', 'images/coloradoRiver.jpg');
+                break;
+            case 'Arivaca Lake ':
+                img.setAttribute('src', 'images/arivacaLake.jpg');
+                break;
+            case 'Dankworth Pond ':
+                img.setAttribute('src', 'images/dankworthPond.jpg');
+                break;
+            case 'Frye Mesa Reservoir ':
+                img.setAttribute('src', 'images/frymesaReservoir.jpg');
+                break;
+            case 'Parker Canyon Lake ':
+                img.setAttribute('src', 'images/parkercanyonLake.jpg');
+                break;
+            case 'Apache Lake ':
+                img.setAttribute('src', 'images/apacheLake.jpg');
+                break;
+            case 'Bartlett Lake ':
+                img.setAttribute('src', 'images/bartlettLake.jpg');
+                break;
+            case 'Canyon Lake ':
+                img.setAttribute('src', 'images/canyonLake.jpg');
+                break;
+            case 'Lake Pleasant ':
+                img.setAttribute('src', 'images/lakePleasant.jpg');
+                break;
+            case 'Lower Salt River ':
+                img.setAttribute('src', 'images/lowersaltRiver.jpg');
+                break;
+            case 'Roosevelt Lake ':
+                img.setAttribute('src', 'images/rooseveltLake.jpg');
+                break;
+            case 'Saguaro Lake ':
+                img.setAttribute('src', 'images/saguaroLake.jpg');
+                break;
+            case 'Tempe Town Lake* ':
+                img.setAttribute('src', 'images/tempetownLake.jpg');
+                break;
+            case 'Bear Canyon Lake ':
+                img.setAttribute('src', 'images/bearcanyonLake.jpg');
+                break;
+            case 'Black Canyon Lake ':
+                img.setAttribute('src', 'images/blackcanyonLake.jpg');
+                break;
+            case 'Chevelon Canyon ':
+                img.setAttribute('src', 'images/chevelonCanyon.jpg');
+                break;
+            case 'Willow Springs Lake ':
+                img.setAttribute('src', 'images/willowspringsLake.jpg');
+                break;
+            case 'Woods Canyon Lake ':
+                img.setAttribute('src', 'images/woodscanyonLake.jpg');
+                break;
+            case 'Becker Lake ':
+                img.setAttribute('src', 'images/beckerLake.jpg');
+                break;
+            case 'Big Lake ':
+                img.setAttribute('src', 'images/bigLake.jpg');
+                break;
+            case 'Greer Lakes ':
+                img.setAttribute('src', 'images/greerLakes.jpg');
+                break;
+            case 'Carnero Lake ':
+                img.setAttribute('src', 'images/carneroLake.jpg');
+                break;
+            default:
+                console.log('default switch executed');
+                img.setAttribute('src', 'images/saguaroLake.jpg');
+        }
+
+
+        img.setAttribute('class', 'card-img-top');
+        cbody = document.createElement('div');
+        cbody.setAttribute('class', 'card-body');
+        h5 = document.createElement('h5');
+        h5.setAttribute('class', 'card-title');
+        h5.textContent = entry.name;
+        p1 = document.createElement('p');
+        p1.setAttribute('class', 'card-text');
+        p1.textContent = 'Region: ' + entry.region;
+        p2 = document.createElement('p');
+        p2.setAttribute('class', 'card-text');
+        p2.textContent = 'Fishing rating: ' + entry.rating;
+
+
+        card.appendChild(img);
+        cbody.appendChild(h5);
+        cbody.appendChild(p1);
+        cbody.appendChild(p2);
+        card.appendChild(cbody);
+
+        contentArea = document.getElementById('content');
+
+        contentArea.appendChild(card);
+        // Add line break to differentiate cards
+        br = document.createElement('br');
+        contentArea.appendChild(br);
+        reset_allData();
+    }
+}
+
+function update_search(selection) {
+    switch(selection) {
+        case 'name':
+            console.log("name selected");
+            order_by_name();
+            break;
+        case 'region':
+            console.log("region selected");
+            order_by_region();
+            break;
+        case 'rating':
+            console.log("rating selected");
+            order_by_rating();
+            break;
+        default:
+            console.log('default case for update_search function');
+        }
 }
